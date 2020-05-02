72053209747ed0a015a9cb91099f2eeaf6a22f02
Saloni Shah <shahsalonik@gmail.com>
2020-05-02 01:19:59 -0400

    New updates for Heroku deployment

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -1,12 +1,10 @@
 """
 WSGI config for personal_portfolio project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-import django
+
 import os
 
 from django.core.wsgi import get_wsgi_application
